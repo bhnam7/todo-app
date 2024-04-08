bfac8f804d444d84d928bf3d2c0028f093dcdd01
Nam <n.buihoai@gmail.com>
2024-04-08 21:47:38 +0200

    comments

--- functions.py
+++ functions.py
@@ -17,7 +17,7 @@ def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
-print(__name__)
+# print(__name__)
 if __name__ == "__main__":
     print("hello")
     print(get_todos())
